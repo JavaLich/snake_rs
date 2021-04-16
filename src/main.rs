d0baad8430fcf6628334c8bd4fd0dba150dfb5bb
Akash Melachuri <amelachuri@gmail.com>
2021-04-16 00:33:56 -0400

    Movement and rendering snake head

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
 use bevy::prelude::*;
 
-pub struct HelloPlugin;
-
-struct GreetTimer(Timer);
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app
-            .add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_system(hello_world.system());
-    }
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
 }
 
-fn hello_world(time: Res<Time>, mut timer: ResMut<GreetTimer>) {
-    if timer.0.tick(time.delta_seconds()).just_finished() {
-        return;
-    }
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
 
-    println!("hello world");
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead);
+}
+
+fn snake_movement(keyboard_input: Res<Input<KeyCode>>, mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::S) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            transform.translation.y += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            transform.translation.x -= 2.;
+        }
+    }
 }
 
 fn main() {
     App::build()
+        .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
+        .add_system(snake_movement.system())
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
         .run();
 }
